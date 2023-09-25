1a0800cbfd1098221a474db836958ba8108f3067
Mohit Ganna <mohitganna@gmail.com>
2023-09-25 10:20:36 -0400

    Created login page (without design)

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=16)
+    number = models.CharField(max_length=64)
+    semester = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.number}"
