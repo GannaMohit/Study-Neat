2650dfaeaec13f64f745c7bb6c13d264500d349e
Mohit Ganna <mohitganna@gmail.com>
2023-10-11 00:31:31 -0400

    Created Task model

--- courses/models.py
+++ courses/models.py
@@ -8,3 +8,22 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.number}"
+    
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(choices=[
+       ( "Assignment", "Assignment"),
+       ("Reading", "Reading"),
+       ("Quiz", "Quiz"),
+       ("Exam", "Exam")
+    ])
+    file = models.FileField(blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='tasks')
+
+    #Attributes
+    difficulty = models.IntegerField(min=1, max=10)
+    estimated_time = models.IntegerField() #In Minutes
+    priority = models.IntegerField(min=1, max=5)
+
+    def __str__(self):
+        return f"{self.name}"
