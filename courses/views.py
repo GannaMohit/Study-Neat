a1149e9ef8edba75792b7674a6507bf4daaf9b36
Mohit Ganna <mohitganna@gmail.com>
2023-11-08 10:57:00 -0500

    Created Course Update View

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from .forms import CourseForm
 from .models import Course
 
 # Create your views here.
 class CourseCreateView(CreateView):
+    template_name = "courses/course_form.html"
+    model = Course
+    form_class = CourseForm
+
+class CourseUpdateView(UpdateView):
     template_name = "courses/course_form.html"
     model = Course
     form_class = CourseForm
