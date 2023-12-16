ed1915e816aebbfe062c4a6c0a4538b62008ea01
Mohit Ganna <mohitganna@gmail.com>
2023-12-16 02:40:18 -0500

    Few changes in HTML

--- masters/urls.py
+++ masters/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from masters.views import DashboardView
 
 urlpatterns = [
     path("", LoginView.as_view(template_name='masters/login.html'), name='login'),
-    path("dashboard", DashboardView.as_view(), name='dashboard')
+    path("dashboard", DashboardView.as_view(), name='dashboard'),
+    path("", LogoutView.as_view(), name='logout')
 ]
--- masters/views.py
+++ masters/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from masters.forms import TodayForm
+from schedule.models import Week_Task, Week
 
 # Create your views here.
-class DashboardView(TemplateView):
+class DashboardView(ListView):
     template_name = 'masters/dashboard.html'
+    model = Week_Task
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
